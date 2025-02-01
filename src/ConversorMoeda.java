b4a43aa89239721de9c147e5593dd58bcce03583
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-02-01 15:56:15 -0300

    Update ConversorMoeda.java

--- src/ConversorMoeda.java
+++ src/ConversorMoeda.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+public class ConversorMoeda implements ConversaoFinanceira{
 
-public class ConversorMoeda {
+    public void converterDolarParaReal(double valorDolar) {
+        double cotacaoDolar = 4.80;
+        double valorReal = valorDolar * cotacaoDolar;
+        System.out.println("O valor em reais é: R$" + valorReal);
+    }
 }
