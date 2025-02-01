84473f8f73889090894c9ba8eb6a0a272c344624
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-02-01 15:56:00 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ConversorMoeda conversor = new ConversorMoeda();
+        conversor.converterDolarParaReal(50);
     }
 }
